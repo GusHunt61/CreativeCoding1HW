ee40f20072e6eb37701f11a970701761e13a461c
GusHunt61 <gus.hunt@umconnect.umt.edu>
2025-03-30 13:12:12 -0700

    Create SelfPortrait.js

---
+++ Week9/SelfPortrait.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(400,400);
+  }
+  
+  function draw() {
+    background(200, 20, 100);
+    circle(200,200,100);
+    ellipse(200,350,100,200);
+    circle(180, 200, 10);
+    circle(220, 200, 10);
+    line(180,240,220,240);
+    triangle(100,400,170,275,120,400);
+    triangle(300,400,230,275,320,400);
+    rect(190,260,20,100);
+    point(200,220);
+    text('Gus Hunt', 340, 390);
+  }
