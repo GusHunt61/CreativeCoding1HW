105a63cbc27b8caa2b22361062bd8a39b338c4eb
GusHunt61 <gus.hunt@umconnect.umt.edu>
2025-04-14 19:37:08 -0700

    Update Sketch.js

--- Week11/Sketch.js
+++ Week11/Sketch.js
@@ -1,8 +1,36 @@
-<html>
-<head>
-<script src="movingSelfPortrait.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/p5@1.11.3/lib/p5.min.js"></script>
-</head>
-<body></body>
+var x = 50;
+var y = 300;
+var diameter = 25;
+function setup() {
+  createCanvas(800, 600);
+}
+function draw() {
+  background(0);
+  fill(250);
+  circle(x, y, diameter);
+  text('EXIT->', 750,300);
+  fill(0, 0, 255);
+  square(200,100,20);
+  fill(0,225,0);
+  rect(500,400,10,30);
 
-</html>
+  if (keyIsDown(68)) {
+    x ++;
+  }
+  if (keyIsDown(65)) {
+    x --;
+  }
+  if (keyIsDown(83)) {
+    y ++;
+  }
+  if (keyIsDown(87)) {
+    y --;
+  }
+  fill(225);
+  if (x >= 800){
+    text('you win', 400, 300);
+  }
+    if (x <= 0){
+    text('wrong way', 400, 300);
+  }
+}
