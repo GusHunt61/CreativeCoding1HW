5d51e7f085d2f5b94e643185e2c7753969924adb
GusHunt61 <gus.hunt@umconnect.umt.edu>
2025-04-19 21:10:54 -0700

    Update Sketch.js

--- Week12/Sketch.js
+++ Week12/Sketch.js
@@ -22,62 +22,63 @@ function draw() {
   moveObstacles();
   obstacleParameters();
   movePlayer(5);
-  function makePlayer(diameter) {
-    fill(255);
-    circle(x, y, diameter);
-  }
+  exits();
+}
+function makePlayer(diameter) {
+  fill(255);
+  circle(x, y, diameter);
+}
 
-  function movePlayer(moveAmount) {
-    if (keyIsDown(68)) {
-      x += moveAmount;
-    }
-    if (keyIsDown(65)) {
-      x -= moveAmount;
-    }
-    if (keyIsDown(83)) {
-      y += moveAmount;
-    }
-    if (keyIsDown(87)) {
-      y -= moveAmount;
-    }
+function movePlayer(moveAmount) {
+  if (keyIsDown(68)) {
+    x += moveAmount;
   }
-  function makeObstacles() {
-    fill(0, 0, 255);
-    square(altx, alty, 20);
-    fill(0, 225, 0);
-    rect(altx, 400, 10, 30);
+  if (keyIsDown(65)) {
+    x -= moveAmount;
   }
-
-  function moveObstacles() {
-    altx += randomVal;
-    alty += randomVal2;
+  if (keyIsDown(83)) {
+    y += moveAmount;
   }
-  function obstacleParameters() {
-    if (alty > 600) {
-      alty = 0;
-    }
-    if (altx > 800) {
-      altx = 0;
-    }
-    if (alty < 0) {
-      alty = 600;
-    }
-    if (altx < 0) {
-      altx = 800;
-    }
-  }
-  function createBorders(width, height, thickness) {
-    rect(0, 0, width, thickness);
-
-    rect(0, height - thickness, width, thickness);
-  }
-  function mouseClicked() {
-    mousex = mouseX;
-    mousey = mouseY;
+  if (keyIsDown(87)) {
+    y -= moveAmount;
   }
 }
-function exits(){
-    if (x >= 800) {
+function makeObstacles() {
+  fill(0, 0, 255);
+  square(altx, alty, 20);
+  fill(0, 225, 0);
+  rect(altx, 400, 10, 30);
+}
+
+function moveObstacles() {
+  altx += randomVal;
+  alty += randomVal2;
+}
+function obstacleParameters() {
+  if (alty > 600) {
+    alty = 0;
+  }
+  if (altx > 800) {
+    altx = 0;
+  }
+  if (alty < 0) {
+    alty = 600;
+  }
+  if (altx < 0) {
+    altx = 800;
+  }
+}
+function createBorders(width, height, thickness) {
+  rect(0, 0, width, thickness);
+
+  rect(0, height - thickness, width, thickness);
+}
+function mouseClicked() {
+  mousex = mouseX;
+  mousey = mouseY;
+}
+function exits() {
+  if (x >= 800) {
     text("you win", 400, 300);
   }
   if (x <= 0) {
